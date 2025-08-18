7fe40b5343eb85fa942cb224af83cc8816b09788
worshipperfx <chitengamarvellous@gmail.com>
2025-08-18 14:38:21 +0200

    Add CORS logic

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend.config import settings
 from backend.routers import upload, chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Sonia Conversations API",
@@ -13,3 +14,16 @@ def root():
 # register routes
 app.include_router(upload.router, prefix="/api")
 app.include_router(chat.router, prefix="/api")
+
+origins = [
+    "http://localhost:5173",
+    "https://<your-vercel-domain>.vercel.app",   # add after you deploy frontend
+    "https://<your-custom-domain>"               # optional
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
